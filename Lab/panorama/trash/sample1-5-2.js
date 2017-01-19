dcf9f6f7185b9528bd677e8c006264451e58127f
ShoichiMidorikawa <midori@aomori-u.ac.jp>
2017-01-19 17:07:09 +0900

    add

---
+++ Lab/panorama/trash/sample1-5-2.js
@@ -0,0 +1,111 @@
+﻿$(function(){
+
+/* ---   写真のデータ入力 ----  */
+
+      ph = 1110;   // 写真の高さ
+      pw = 8192;   // 写真の幅
+      pr = pw/ph;
+
+
+
+   var photo = document.getElementById('photo'); 
+
+
+   // img要素をHTLに追加
+   var img;
+      img = document.createElement('img');
+      img.src = "panorama.jpg";  
+      img.alt="ピロティ";
+      img.id="panorama"; 
+//      img.vspace="50";
+      img.style.display = 'none';
+
+   var img2;
+      img2 = document.createElement('img');
+      img2.src = "panorama.jpg";  
+      img2.alt="ピロティ";
+//      img2.vspace="50";
+      img.id="panorama"; 
+      img2.style.display = 'none';
+
+   var img3;
+      img3 = document.createElement('img');
+      img3.src = "panorama.jpg";  
+      img3.alt="ピロティ";
+//      img3.vspace="50";
+      img.id="panorama"; 
+      img3.style.display = 'none';
+
+
+      img.height = 350;
+      img.width = Math.round(img.height*pr); 
+
+      img2.height = 350;
+      img2.width = Math.round(img.height*pr); 
+
+      img3.height = 350;
+      img3.width = Math.round(img.height*pr); 
+
+
+      plm = 3*pr*img.height-600;  // left-margin
+
+      console.log("width="+Math.round(3*pr*img.height)) // cssの値
+      console.log("height="+img.height);  // cssの値
+
+
+      photo.appendChild(img);
+
+      photo.appendChild(img2);
+
+      photo.appendChild(img3);
+
+
+   title.innerHTML = img.alt;   
+   img.style.display = 'inline';   
+
+   title.innerHTML = img2.alt;   
+   img2.style.display = 'inline';   
+
+   title.innerHTML = img3.alt;   
+   img3.style.display = 'inline';   
+
+
+
+// mousemove
+   var xm, ym;
+   var xi, yi;
+   var xf, yf; 
+   var xd;
+   var xl;
+   var xl=-2100; //cssファイルのmargin-leftの値 
+  
+     console.log("margin-left="+xl); // cssファイルのmargin-leftの値 
+
+
+     $("#photo").mousedown(function(e){
+        console.log("mouse down");
+        xi=e.clientX;
+        yi=e.clientY;  
+     });
+   
+
+    $("#photo").mouseup(function(evt){
+        console.log("mouse up");
+       xf=evt.clientX;
+       yf=evt.clientY;
+       xd =xf-xi;
+       xl +=xd;
+       if(xl<-plm) xl=-plm; 
+       if(xl>0) xl=0;
+       console.log("xl="+xl);
+       $("p:not(:animated)").animate({
+           "margin-left": xl
+       },"slow", "swing");
+    });
+
+
+
+
+});
+
+
