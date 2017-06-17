b076b4a2acb230d80faf4b7980928821cb54f413
ShoichiMidorikawa <midori@aomori-u.ac.jp>
2017-06-17 19:20:53 +0900

    add and change

---
+++ Lab/HTMLtutorial/sample6.js
@@ -0,0 +1,50 @@
+﻿onload= function(){
+  var sl=document.getElementById('sel1');
+  var image=document.getElementById('image');
+  
+  sl.onchange = function(){
+    var selectedItem = this.options[this.selectedIndex];
+
+     switch (selectedItem.value){
+        case '0': image.src="https://www.youtube.com/embed/ulStCvohAHk?rel=0" ; break;
+        case '1': image.src="https://www.youtube.com/embed/KbB0FjPg0mw?rel=0" ; break;
+        case '2': image.src="https://www.youtube.com/embed/FJd_1H3rZGg?rel=0" ; break;
+        case '3': image.src="https://www.youtube.com/embed/LZ5Wergp_PA?rel=0" ; break; 
+        case '4': image.src="https://www.youtube.com/embed/P7NE4WF8j-Q?rel=0" ; break;
+        case '5': image.src="https://www.youtube.com/embed/JzDvVgNDxo8?rel=0" ; break;
+        case '6': image.src="https://www.youtube.com/embed/fDcjhAKuhqQ?rel=0" ; break;
+        case '7': image.src="https://www.youtube.com/embed/PNrqCdslGi4?rel=0" ; break;
+        case '8': image.src="https://www.youtube.com/embed/k2BB0p8byGA?rel=0" ; break;
+        case '9': image.src="https://www.youtube.com/embed/LX2q356N2rU?rel=0" ; break;
+        case '10':image.src="https://www.youtube.com/embed/P1fSFvhPf7Q?rel=0" ; break;
+        case '11':image.src="https://www.youtube.com/embed/TD1N4hxqMzY?rel=0" ; break;
+        case '12':image.src="https://www.youtube.com/embed/Tci---bVs60?rel=0" ; break;
+        case '13':image.src="https://www.youtube.com/embed/72QjzHnYvL0?rel=0" ; break;
+        case '14':image.src="https://www.youtube.com/embed/9vp1Ll2NpRw?rel=0" ; break;
+        case '15':image.src="https://www.youtube.com/embed/yFRZf81sB5k?rel=0" ; break;
+        case '16':image.src="https://www.youtube.com/embed/bM6nFDjvEns?rel=0" ; break;
+        case '17':image.src="https://www.youtube.com/embed/N8O6zd6vTZ8?rel=0" ; break;
+        case '18':image.src="https://www.youtube.com/embed/tVDdx6xUOcs?rel=0" ; break;
+        case '19':image.src="https://www.youtube.com/embed/J70dP_AECzQ?rel=0" ; break;
+        case '20':image.src="https://www.youtube.com/embed/xiVWNkQUqKk?rel=0" ; break;
+        case '21':image.src="https://www.youtube.com/embed/IujCYxtpszU?rel=0" ; break;
+        case '22':image.src="https://www.youtube.com/embed/yXwPUAIvFyg?rel=0" ; break;
+        case '23':image.src="https://www.youtube.com/embed/UZjlBQbV1KU?rel=0" ; break;
+        case '24':image.src="https://www.youtube.com/embed/Qjeswpm0cWY?rel=0" ; break;
+        case '25':image.src="https://www.youtube.com/embed/2LR5JYbhyjg?rel=0" ; break;
+        case '26':image.src="https://www.youtube.com/embed/PgawcWisb0I?rel=0" ; break;
+        case '27':image.src="https://www.youtube.com/embed/gjBvCiRt8QA?rel=0" ; break;
+        case '28':image.src="https://www.youtube.com/embed/UtXK_EQ3Pow?rel=0" ; break;
+        case '29':image.src="https://www.youtube.com/embed/OprNqnHsVIA?rel=0" ; break;
+        case '30':image.src="https://www.youtube.com/embed/MF-XSJOsGqw?rel=0" ; break;
+        case '31':image.src="https://www.youtube.com/embed/8AJPs3gvNlY?rel=0" ; break;
+        case '32':image.src="https://www.youtube.com/embed/aBGOyZv2pZE?rel=0" ; break;
+        case '33':image.src="https://www.youtube.com/embed/Q-pCzTpwPBU?rel=0" ; break;
+        case '34':image.src="https://www.youtube.com/embed/ChS3K2O-h7o?rel=0" ; break;
+        case '35':image.src="https://www.youtube.com/embed/dzFf3r1yph8?rel=0" ; break;
+     }
+  }
+};
+
+
+
