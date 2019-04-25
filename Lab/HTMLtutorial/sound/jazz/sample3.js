38f7534040b3aa7f2aa239864e0cbf253adfd623
ShoichiMidorikawa <midori@aomori-u.ac.jp>
2019-04-25 18:32:35 +0900

    change

--- Lab/HTMLtutorial/sound/jazz/sample3.js
+++ Lab/HTMLtutorial/sound/jazz/sample3.js
@@ -4,40 +4,37 @@
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  function onYouTubeIframeAPIReady() {
-      ytPlayer1 = new YT.Player(
-         'sample1', // 埋め込む場所
-         {
-            width: '0',  // プレーヤー幅
-            height: '0', // プレーヤー高さ
-            videoId: 'Tn3acz2KI9M'  // YouTubeID
-         }
-      );
-      ytPlayer2 = new YT.Player(
-             'sample2', 
+var ytID = 'Tn3acz2KI9M';
+var ytID = 'E7X86QlQFQQ'; 
+
+       function onYouTubeIframeAPIReady() {
+          ytPlayer1 = new YT.Player(
+             'sample1', // 埋め込む場所の指定
              {
-                width: '0', 
-                height: '0',
-                videoId: 'KV8Hj_E8LJc'
+                width: '0', // プレーヤーの幅
+                height: '0', // プレーヤーの高さ
+                videoId: 'Tn3acz2KI9M'  // YouTubeのID
              }
-      );
-      ytPlayer3 = new YT.Player(
+          );
+          ytPlayer2 = new YT.Player(
+             'sample2', // 埋め込む場所の指定
+             {
+                width: '0', // プレーヤーの幅
+                height: '0', // プレーヤーの高さ
+                videoId: 'E7X86QlQFQQ' // YouTubeのID
+             }
+          );
+          ytPlayer3 = new YT.Player(
              'sample3', // 埋め込む場所の指定
              {
-                width: '0', 
-                height: '0', 
-                videoId: 'omjZ73GFPaQ' 
+                width: '0', // プレーヤーの幅
+                height: '0', // プレーヤーの高さ
+                videoId: 'omjZ73GFPaQ' // YouTubeのID
              }
-      );
-      ytPlayer4 = new YT.Player(
-             'sample4', // 埋め込む場所の指定
-             {
-                width: '0', 
-                height: '0', 
-                videoId: 'YsVJuulCmAE' 
-             }
-      );
-  };
+          );
+       };
+
+
 
 
 onload = function(){
@@ -53,17 +50,19 @@ onload = function(){
          ytPlayer1.pauseVideo().seekTo(0);
        });
 
+
   sl.onchange = function(){
 
+
+    var selectedItem = 0;
     var selectedItem = this.options[this.selectedIndex];
-  
+ 
+ 
     if(selectedItem.value == 0){
        $('#play').click(function() {
          ytPlayer2.pauseVideo();
          ytPlayer3.pauseVideo();
-         ytPlayer4.pauseVideo();
          ytPlayer1.playVideo();
-   ytPlayer1.playVideo();
        });
        $('#pause').click(function() {
          ytPlayer1.pauseVideo();
@@ -75,7 +74,6 @@ onload = function(){
        $('#play').click(function() {
          ytPlayer1.pauseVideo();
          ytPlayer3.pauseVideo();
-         ytPlayer4.pauseVideo();
          ytPlayer2.playVideo();
        });
        $('#pause').click(function() {
@@ -88,7 +86,6 @@ onload = function(){
        $('#play').click(function() {
          ytPlayer1.pauseVideo();
          ytPlayer2.pauseVideo();
-         ytPlayer4.pauseVideo();
          ytPlayer3.playVideo();
        });
        $('#pause').click(function() {
@@ -96,20 +93,8 @@ onload = function(){
        });
        $('#stop').click(function() {
          ytPlayer3.pauseVideo().seekTo(0);
-       });    
-    }else if(selectedItem.value == 3){
-       $('#play').click(function() {
-         ytPlayer1.pauseVideo();
-         ytPlayer2.pauseVideo();
-         ytPlayer3.pauseVideo();
-         ytPlayer4.playVideo();
-       });
-       $('#pause').click(function() {
-         ytPlayer4.pauseVideo();
-       });
-       $('#stop').click(function() {
-         ytPlayer4.pauseVideo().seekTo(0);
-       });    
+       });
+    
     }
   }
 
