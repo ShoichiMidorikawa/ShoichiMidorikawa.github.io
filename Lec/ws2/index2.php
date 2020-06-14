47c5593caf196dfb6209fd557b1ee38be90fd3c1
ShoichiMidorikawa <midori@aomori-u.ac.jp>
2020-06-15 08:36:09 +0900

    add

---
+++ Lec/ws2/index2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>最初のHTML</title>
+    </head>
+    <body>
+        <h1>サンプルHTML</h1><br>
+        
+        
+        <?php
+            echo "Hello, world!"
+        ?>
+    </body>
+</html>
